eb7b889cc7943544622123b6a708f46c96c80a7e
swainsonsujana <133630639+swainsonsujana@users.noreply.github.com>
2025-02-17 11:17:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="HiResCAM",  # Change this to your package name
-    version="1.0.0",  # Versioning for updates
+    name="HiResCAM", 
+    version="1.0.0", 
     author="D. Swainson Sujana and  D Peter Augustine",
     author_email="d.sujana@res.christuniversity.in",
     description="High-Resolution XAI Explanations for Deep Learning-Based Autism Diagnosis Using Structural MRI Images",
-    long_description=open("README.md").read(),  # Reads the README file for package details
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://swainsonsujana.github.io/XAI_HiResCAM",  # Update with your repo URL
-    packages=find_packages(),  # Finds all Python packages in the directory
+    url="https://swainsonsujana.github.io/XAI_HiResCAM", 
+    packages=find_packages(),  
     install_requires=[
-        "tensorflow>=2.16.1",  # Ensure TensorFlow version compatibility
+        "tensorflow>=2.16.1",  
         "keras>=3.3.3",
         "numpy",
         "scikit-learn",
         "matplotlib",
-        "nibabel>=5.2.1",  # If working with neuroimaging data
-        "opencv-python",  # If using image processing
+        "nibabel>=5.2.1",  
+        "opencv-python", 
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.10.14",  # Ensures Python version compatibility
+    python_requires=">=3.10.14",  
 )
